01b2f28ba36409d7eab1dd53707b6aa758ea8b2b
Aniruddha Nath <aniruddhanath@HAPPAYBLRMAC1604.local>
2023-10-20 07:23:01 +0530

    demo code for kafka producer & consumer

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type OrderPlacer struct {
+	producer   *kafka.Producer
+	topic      string
+	deliverych chan kafka.Event
+}
+
+func NewOrderPlacer(p *kafka.Producer, topic string) *OrderPlacer {
+	return &OrderPlacer{
+		producer:   p,
+		topic:      topic,
+		deliverych: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderPlacer) placeOrder(orderType string, size int) error {
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &op.topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: payload,
+	},
+		op.deliverych,
+	)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	<-op.deliverych
+	fmt.Printf("Placed order in the queue: %s\n", format)
+	return nil
+}
+
+func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "foo",
+		"acks":              "all",
+	})
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+	}
+	fmt.Printf("%+v\n", p)
+
+	op := NewOrderPlacer(p, "HVSE")
+	for i := 0; i < 1000; i++ {
+		if err := op.placeOrder("market", i+1); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second * 3)
+	}
+}
